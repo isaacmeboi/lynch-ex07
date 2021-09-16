d1f03ebb7f7789b3313c2251bd5c1b7e81b171ba
isaac lynch <thebunnyarcher@gmail.com>
2021-09-16 15:18:06 -0400

    5th commit: fixed so everything isnt in main

--- src/main/java/exercise07/solution07.java
+++ src/main/java/exercise07/solution07.java
@@ -7,28 +7,41 @@ package exercise07;
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
-public class solution07 {
+class Dimensions {
 
-    private static DecimalFormat df = new DecimalFormat("0.000");
+    private static DecimalFormat df = new DecimalFormat("0.00");
 
-    public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
+    double length;
+    double width;
 
-            System.out.print("What is the length of the room in feet?");
-            double l = sc.nextDouble();
-            System.out.print("What is the width of the room in feet?");
-            double w = sc.nextDouble();
-            System.out.println("You entered dimension of "+ l +" by "+ w +" feet.");
+    public void scn() {
+        Scanner sc = new Scanner(System.in);
 
-
-            double a = l*w;
-            final double finalAnswer = a*0.09290304;
-
-            System.out.println("The area is:");
-            System.out.println(a +" square feet.");
-            System.out.println(df.format(finalAnswer) +" square meters.");
-        }
+        System.out.print("What is the length of the room in feet?");
+        double length = sc.nextDouble();
+        System.out.print("What is the width of the room in feet?");
+        double width = sc.nextDouble();
+        System.out.println("You entered dimension of "+ length +" by "+ width +" feet.");
     }
+
+    public void prt() {
+        double a = length*width;
+        final double finalAnswer = a*0.09290304;
+
+        System.out.println("The area is:");
+        System.out.println(a +" square feet.");
+        System.out.println(df.format(finalAnswer) +" square meters.");
+    }
+}
+public class solution07 {
+    public static void main(String[] args) {
+
+        Dimensions ca = new Dimensions();
+        ca.scn();
+        ca.prt();
+    }
+}
+
 /* psuedocode: (had to rewrite this bc im a perfectionist :pensive:) first thing is to
 import both the java scanner functions and the java decimal functions. after that, the
 decimalformat function is called into a private static field and is set so that the final
@@ -39,4 +52,7 @@ values are stored into a double so that the final answer can actually have decim
 the area is also stored into a double called "a" which is then used for the finalAnswer by
 multiplying it by "0.09290304" to get the area of the room in square meters. the code then
 outputs both the area in square feet and square meters.
+
+edit: had to rewrite it again lol. put all of the functions into their own separate public
+voids so that the entire program wasn't running in the main.
  */
